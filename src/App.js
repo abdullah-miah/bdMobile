7a8323fe66e0ee06efe1fbb79acc9cba4f60e920
abdullah-miah <mdabdullaha708@gmail.com>
2022-07-25 09:01:34 +0600

    some edit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import './App.css';
-import MobileView from './features/Mobile/MobileView';
-import SmartPhone from './features/Mobile/SmartPhone';
 import { Routes, Route } from "react-router-dom";
 import Login from './Componets/Login/Login';
-import Navbar from './Componets/Home/Navbar';
 import Home from './Componets/Home/Home';
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path='/'  element={<Home></Home>} ></Route>
+        <Route path="/"  element={<Home></Home>} ></Route>
         <Route path="login" element={<Login></Login>}  />
       </Routes>
     </div>
